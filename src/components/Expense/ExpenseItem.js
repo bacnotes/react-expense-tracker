513bde207c394f77325ff9fa8d92240e11df48d8
bacnotes <bacnotes.github.io>
2022-01-05 12:56:04 +0800

    feat/expense-date, fix/date order

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,16 +1,9 @@
-
+import ExpenseDate from './ExpenseDate'
 const ExpenseItem = (props) => {
-  const year = props.date.toLocaleString('en-us', { day: '2-digit' });
-  const month = props.date.toLocaleString('en-us',{month:'long'})
-  const day = props.date.getFullYear()
 
   return (
     <div className='expense-item'>
-      <div>
-        <div>{year}</div>
-        <div>{month}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
         <h2 className='expense-item__name'>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
