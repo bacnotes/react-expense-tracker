a804bd759825122ec062bdad311619a9e62eabb9
bacnotes <bacnotes.github.io>
2022-01-06 00:07:42 +0800

    feat/not found msg

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -2,13 +2,13 @@ import ExpenseDate from './ExpenseDate';
 import Container from './Container';
 const ExpenseItem = (props) => {
   return (
-    <div className='expense-item'>
+    <li className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2 className='expense-item__name'>{props.title}</h2>
         <Container className='expense-item__price'>${props.amount}</Container>
       </div>
-    </div>
+    </li>
   );
 };
 
--- src/components/Expense/ExpenseList.js
+++ src/components/Expense/ExpenseList.js
@@ -1,30 +1,38 @@
 import ExpenseItem from './ExpenseItem';
 import Container from './Container';
-import ExpensesFilter from './../ExpensesFilter/ExpensesFilter'
-import { useState } from 'react'
+import ExpensesFilter from './../ExpensesFilter/ExpensesFilter';
+import { useState } from 'react';
 
 const ExpenseList = (props) => {
-  const [year, setYear] = useState(2022);  
+  const [year, setYear] = useState(2022);
   const filterHandler = (dropdownYear) => {
-    setYear(dropdownYear)
+    setYear(dropdownYear);
+  };
+
+  const filteredList = props.items.filter(
+    (el) => el.date.getFullYear() === Number(year)
+  );
+
+  let content = <h2 className ='expense-list__notfound'>No expenses found</h2>;
+
+  if (filteredList.length > 0) {
+    content = filteredList.map((el) => (
+      <ExpenseItem
+        key={el.id}
+        title={el.title}
+        amount={el.amount}
+        date={el.date}
+      />
+    ));
   }
 
-  const filteredList = props.items.filter(el => el.date.getFullYear() === Number(year));
-
   return (
-    <div>
+    <ul>
       <Container className='new-expense'>
         <ExpensesFilter selected={year} onChangeFilter={filterHandler} />
-        {filteredList.map((el) => (
-          <ExpenseItem
-            key={el.id}
-            title={el.title}
-            amount={el.amount}
-            date={el.date}
-          />
-        ))}
+        {content}
       </Container>
-    </div>
+    </ul>
   );
 };
 
