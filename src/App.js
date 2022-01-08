9329d25863e3b370594dcc8c7b39847609027cf2
bacnotes <bacnotes.github.io>
2022-01-09 01:10:53 +0800

    feat/add logout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Switch, Route, Redirect} from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import AuthContext from './store/auth-context';
 import './scss/main.scss';
 import AuthPage from './pages/AuthPage';
@@ -7,18 +7,22 @@ import ExpensePage from './pages/ExpensePage';
 import Navbar from './layouts/Navbar';
 const App = () => {
   const authCtx = useContext(AuthContext);
-  const isLoggedIn = authCtx.isLoggedIn
+  const isLoggedIn = authCtx.isLoggedIn;
+  // 首頁為 originUrl/react-expense-tracker
   return (
     <div>
       <Navbar />
       <Switch>
         <Route path='/' exact>
-          {isLoggedIn ? <Redirect to='/react-expense-tracker'/> : <AuthPage />}
+          {isLoggedIn ? <Redirect to='/react-expense-tracker' /> : <AuthPage />}
         </Route>
         <Route path='/react-expense-tracker'>
-          <ExpensePage />
+          {isLoggedIn ? <ExpensePage /> : <AuthPage />}
         </Route>
         <Route path='/auth'>
+          <AuthPage />
+        </Route>
+        <Route path='*'>
           <AuthPage />
         </Route>
       </Switch>
--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -4,7 +4,11 @@ import AuthContext from './../store/auth-context';
 
 const Navbar = () => {
   const authCtx = useContext(AuthContext);
-  const isLoggedIn = authCtx.isLoggedIn
+  const isLoggedIn = authCtx.isLoggedIn;
+  const logoutHandler = () => {
+    authCtx.logout();
+  };
+
   return (
     <Container className='navbar'>
       <h1 className='navbar__title'>Expense Tracker</h1>
@@ -12,7 +16,9 @@ const Navbar = () => {
         <ul>
           {isLoggedIn && (
             <li>
-              <button className='navbar__logout'>Logout</button>
+              <button className='navbar__logout' onClick={logoutHandler}>
+                Logout
+              </button>
             </li>
           )}
         </ul>
